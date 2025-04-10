1016b14647195e9177f88e45626ade0e2a048245
Mariyan Donchev <pi4aaa@abv.bg>
2025-04-10 19:11:48 +0300

    add page routs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import GameList from "./components/gameList/GameList";
+import GameCreate from "./components/gameCreate/GameCreate";
 
 function App() {
   return (
@@ -7,8 +13,13 @@ function App() {
       <Header />
 
       <main id="main-content">
-        <Home/>
-        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/games" element={<GameList />} />
+          <Route path="/games/create" element={<GameCreate />} />
+        </Routes>
       </main>
     </div>
   );
